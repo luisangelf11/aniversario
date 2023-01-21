60989b41e2679b65dba9f8dba9867d7b40771b8f
luisangelf11 <71238653+luisangelf11@users.noreply.github.com>
2023-01-21 11:38:03 -0400

    Zona de cupones

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 const btn = document.getElementById("card-btn"),
       info = document.getElementById("info"),
       btn_two = document.getElementById('card-btn-2'),
-      info_two = document.getElementById('info-2');
+      info_two = document.getElementById('info-2'),
+      btn_three = document.getElementById('card-btn-3'),
+      info_three = document.getElementById('info-3');
 
 function OpenAndCloseCardOne(){
     if(info.classList.contains('none')) info.classList.remove('none');
@@ -13,5 +15,11 @@ function OpenAndCloseCardTwo(){
     else info_two.classList.add('none')
 }  
 
+function OpenAndCloseCardThree(){
+    if(info_three.classList.contains('none')) info_three.classList.remove('none');
+    else info_three.classList.add('none')
+}  
+
 btn.addEventListener("click", OpenAndCloseCardOne);
 btn_two.addEventListener("click", OpenAndCloseCardTwo);
+btn_three.addEventListener("click", OpenAndCloseCardThree);
